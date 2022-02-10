f2e239681f77d776b884db1f4da04ec32feb0a7a
sinanseslikaya <ss6gw@virginia.edu>
2022-02-10 00:13:14 -0500

    added a bunch of functionality, next step is to implement as a bot instead of client

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
 # bot.py
 import os
-
+import random
 import discord
+
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
-
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    guild = discord.utils.get(client.guilds, name=GUILD)
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{client.user.name} has connected to: \n'
         f'{guild.name}(id: {guild.id})'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
+@client.event
+async def on_message(message):
+    if message.author == client.user: 
+     # ensures no recursive loop where bot reacts to its own message 
+        return
+    
+    #TODO: add more dune quotes lol
+    dune_quotes = [
+    "I must not fear. Fear is the mind-killer.",
+    "He who controls the spice controls the universe.",
+    "The people who can destroy a thing, they control it."
+    ]   
+
+    if (message.content).lower() == "dune":
+        response = random.choice(dune_quotes)
+        await message.channel.send(response)
+    if (message.content).lower() == "tatakae":
+        await message.channel.send("tatakae")
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException   
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise   
 
 client.run(TOKEN)
